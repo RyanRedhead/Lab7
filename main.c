e9b4741c9f6cb3d8ce38f1106db5c0b45fbeb06f
RyanRedhead <C16Ryan.Redhead@usafa.edu>
2014-12-12 13:05:12 -0700

    prelab

--- main.c
+++ main.c
@@ -24,9 +24,28 @@ int main(void) {
 	P1DIR |= BIT0;
 	P1DIR |= BIT6;
 
+	P2DIR |= BIT0;
+	P2OUT |= BIT0;
+
+	P2DIR |= BIT1;
+	P2OUT &= ~BIT1;
+
+	P2DIR |= BIT2;							// P2.2 is associated with TA1CCR1
+	P2SEL |= BIT2;							// P2.2 is associated with TA1CCTL1
+
+	P2DIR |= BIT3;
+	P2OUT &= ~BIT3;
+
+	P2DIR |= BIT4;							// P2.4 is associated with TA1CCR2
+	P2SEL |= BIT4;							// P2.4 is associated with TA1CCTL2
+
+	TA1CTL = ID_3 | TASSEL_2 | MC_1;		// Use 1:8 presclar off MCLK
+	TA1CCR0 = 0x0100;						// set signal period
+
+
 	while(1) {
 
-		LeftSample = readvalue(2);
+/*		LeftSample = readvalue(2);
 		CenterSample = readvalue(3);
 		RightSample = readvalue(4);
 
@@ -49,7 +68,7 @@ int main(void) {
 			P1OUT &= ~BIT0;
 			P1OUT &= ~BIT6;
 		}
-
+*/
 
 /*		ADC10CTL0 = 0;											//P1.4
 		ADC10CTL1 = INCH_4 | ADC10DIV_3 ;
@@ -77,10 +96,27 @@ int main(void) {
 		ADC10CTL0 |= ADC10SC;									// Start a conversion
 		while(ADC10CTL1 & ADC10BUSY);							// Wait for conversion to complete
 		CenterSample = ADC10MEM;									// collect that 10-bit value
+*/
+
+		if (CenterSample < 0x200){
+	    	P2OUT |= BIT0;
+	        P2OUT &= ~BIT1;
+	        TA1CCR1 = 0x0070;
+	        TA1CCTL1 = OUTMOD_7;
+
+	        P2OUT &= ~BIT3;
+	        TA1CCTL2 = OUTMOD_7;
+	        TA1CCR2 = 0x0070;
+
+	        __delay_cycles(3000000);
+		}
 
 
 
-		if (LeftSample > 0x200) {
+
+
+
+		/*		if (LeftSample > 0x200) {
 			P1OUT |= BIT6;
 			P1OUT &= ~BIT0;
 		}
